37ed3f5f50e3e25431f726d33162f92606591541
YoungChen <niaomingjian@outlook.com>
2017-03-30 20:13:49 +0800

    getter setter更简洁

--- src/main/java/prop/TheProperties.java
+++ src/main/java/prop/TheProperties.java
@@ -1,40 +1,23 @@
 package prop;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * <pre>
  * </pre>
  */
-//@Data
+@Data
 @Component
 @ConfigurationProperties("myname") // locations = "mappingconfig.yaml")
 public class TheProperties {
   private String name;
 
-  private HashMap<String, String> mappingMap= new HashMap<String, String>();
+  private HashMap<String, String> mappingMap;
 
-  private List<String> servers = new ArrayList<String>();
-
-  public List<String> getServers() {
-    return this.servers;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Map<String, String> getMappingMap() {
-    return mappingMap;
-  }
+  private List<String> servers;
 }
