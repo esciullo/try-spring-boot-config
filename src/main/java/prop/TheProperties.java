1ccfdaab43789bdc2fd15ab85032728a301682b4
YoungChen <niaomingjian@outlook.com>
2017-03-30 20:07:59 +0800

    Loading YAML
    String List Map

--- src/main/java/prop/TheProperties.java
+++ src/main/java/prop/TheProperties.java
@@ -5,18 +5,36 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * <pre>
- * 
  * </pre>
  */
 @Data
 @Component
-@ConfigurationProperties("myname")//locations = "mappingconfig.yaml")
+@ConfigurationProperties("myname") // locations = "mappingconfig.yaml")
 public class TheProperties {
   private String name;
-  //private HashMap<String, ConvertSetting> ex;
-  private List<String> servers = new ArrayList<String>();
+
+  private HashMap<String, String> mappingMap;// = new HashMap<String, String>();
+
+  private List<String> servers;// = new ArrayList<String>();
+
+//  public List<String> getServers() {
+//    return this.servers;
+//  }
+//
+//  public void setName(String name) {
+//    this.name = name;
+//  }
+//
+//  public String getName() {
+//    return name;
+//  }
+//
+//  public Map<String, String> getMappingMap() {
+//    return mappingMap;
+//  }
 }
--- src/test/java/prop/SamplePropertyLoadingTest.java
+++ src/test/java/prop/SamplePropertyLoadingTest.java
@@ -1,9 +1,5 @@
 package prop;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,26 +10,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest(classes = SampleWebApplication.class)
 public class SamplePropertyLoadingTest {
 
-//  @Autowired
-//  private SampleProperty sampleProperty;
-
-//  @Value("${prefix.stringProp1}")
-//  private String stringProp1;
-
   @Autowired
   private TheProperties theProperties;
   
   @Test
   public void testLoadingOfProperties() {
-//    System.out.println(sampleProperty);
-////    System.out.println("stringProp1 = " + stringProp1);
-//    assertThat(sampleProperty.getStringProp1(), equalTo("propValue1custom"));
-//    assertThat(sampleProperty.getStringProp2(), equalTo("propValue2"));
-//    assertThat(sampleProperty.getIntProp1(), equalTo(10));
-//    assertThat(sampleProperty.getListProp(), hasItems("listValue1", "listValue2"));
-////    assertThat(sampleProperty.getMapProp(), allOf(hasEntry("key1", "mapValue1"), hasEntry("key2", "mapValue2")));
-//    
-//    System.out.println(sampleProperty.getMapProp());
     System.out.println(theProperties);
   }
 }
