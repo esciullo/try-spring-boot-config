061b5b5bdfbaf3e3d31b26a81a14f61a7e40889b
YoungChen <niaomingjian@outlook.com>
2017-03-31 13:39:09 +0800

    using spring.config.name to change file name
    spring.config.name = application,viaenvironment
    extension name must be `.yml`

--- src/test/java/prop/SamplePropertyLoadingTest.java
+++ src/test/java/prop/SamplePropertyLoadingTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SampleWebApplication.class, properties = "spring.config.name=application,myconfig")
+@SpringBootTest(classes = SampleWebApplication.class)
 public class SamplePropertyLoadingTest {
 
   @Autowired
