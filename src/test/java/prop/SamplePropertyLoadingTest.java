c41a0d85e03b4a426a573f49f561aa285362c805
YoungChen <niaomingjian@outlook.com>
2017-03-31 09:29:43 +0800

    @SpringBootTest properties中指定spring.config.name
    可以指定yaml不带扩展名的文件名，扩展名必须是.yml，而不是.yaml

--- src/test/java/prop/SamplePropertyLoadingTest.java
+++ src/test/java/prop/SamplePropertyLoadingTest.java
@@ -7,16 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SampleWebApplication.class)
+@SpringBootTest(classes = SampleWebApplication.class, properties = "spring.config.name=application,myconfig")
 public class SamplePropertyLoadingTest {
 
   @Autowired
   private TheProperties theProperties;
-  
+
   @Autowired
   private TheMapProperties theMapProperties;
-  
-  
+
   @Test
   public void testLoadingOfProperties() {
     System.out.println(theProperties);
