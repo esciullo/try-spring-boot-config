b1338b72845bcb8ba43f06d6ca2f90384ec135fe
YoungChen <niaomingjian@outlook.com>
2017-04-07 13:20:52 +0800

    try to use @TestPropertySource

---
+++ src/test/java/prop/SamplePropertyLoadingTestWithTestPropertySource.java
@@ -0,0 +1,22 @@
+package prop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:viaenvironment.yml")
+public class SamplePropertyLoadingTestWithTestPropertySource {
+
+  @Autowired
+  private TheMapProperties theMapProperties;
+
+  @Test
+  public void testLoadingOfProperties() {
+    System.out.println(theMapProperties);
+  }
+}
