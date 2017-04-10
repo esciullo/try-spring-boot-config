9a5cdbbdd03a420442dc8d36509f006320ac8792
YoungChen <niaomingjian@outlook.com>
2017-04-10 10:29:03 +0800

    inspect the beans provided by Spring Boot

--- src/main/java/prop/SampleWebApplication.java
+++ src/main/java/prop/SampleWebApplication.java
@@ -1,9 +1,14 @@
 package prop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 @SpringBootApplication
@@ -16,4 +21,20 @@ public class SampleWebApplication {
         .properties("spring.config.name:application", "spring.config.location=classpath:/viaenvironment.yaml").build()
         .run(args);
   }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+
+      System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+      String[] beanNames = ctx.getBeanDefinitionNames();
+      Arrays.sort(beanNames);
+      for (String beanName : beanNames) {
+        System.out.println(beanName);
+      }
+
+    };
+  }
+
 }
