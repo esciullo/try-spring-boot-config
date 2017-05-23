9191f83c485bbc43c93da92bec7f2c6fec3819d5
YoungChen <niaomingjian@outlook.com>
2017-05-23 19:41:24 +0800

    logback configuration with logback-spring.xml

--- src/main/java/prop/controller/HelloController.java
+++ src/main/java/prop/controller/HelloController.java
@@ -1,14 +1,19 @@
 package prop.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import prop.SampleProperty;
 import prop.TheMapProperties;
+import prop.helpers.SpringLoggingHelper;
 
 @RestController
 public class HelloController {
 
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  
   @Autowired
   private TheMapProperties theMapProperties;
   
@@ -20,6 +25,12 @@ public class HelloController {
     System.out.println(theMapProperties);
     System.out.println("sampleProperty" + sampleProperty);
 
+    logger.debug("This is a debug message");
+    logger.info("This is an info message");
+    logger.warn("This is a warn message");
+    logger.error("This is an error message");
+    new SpringLoggingHelper().helpMethod();
+    
     return "Greetings from Spring Boot!";
   }
 
