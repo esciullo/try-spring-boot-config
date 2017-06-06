dea37d200e7db0837a2a1e399d8a19a4c9aa27b4
YoungChen <niaomingjian@outlook.com>
2017-06-06 19:36:03 +0800

    Revert "Jackson ObjectMapper in spring-boot"
    This reverts commit 4a881c51791a1f7d3355c78d41a0290bbd7bbf14.

--- src/main/java/prop/service/VehicleDetailsService.java
+++ src/main/java/prop/service/VehicleDetailsService.java
@@ -1,11 +1,9 @@
 package prop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import prop.TheMapProperties;
 import prop.utils.StringUtil;
 
-@Component
 public class VehicleDetailsService {
   @Autowired
   private TheMapProperties theMapProperties;
